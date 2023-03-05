1e9fc118284a0a217ef20ac656abca279ba28ed9
André Luis dos Santos <alsss.ads@gmail.com>
2023-03-05 20:30:51 -0300

    feat: limpa projeto e adiciona teste

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import PeopleApi from '@/services/People'
+import {onMounted} from "vue";
+
+import {ref} from "vue";
+
+const teste = ref({})
+
+onMounted(async () =>{
+  teste.value = await PeopleApi.get(1)
+})
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+  <div>
+    {{teste}}
+  </div>
   </main>
 </template>
