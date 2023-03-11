9dcad2ba9b7adad87733ed7dbec747c205e250e6
André Luis dos Santos <alsss.ads@gmail.com>
2023-03-10 22:30:19 -0300

    feat: adiciona teste de lista e busca de personagem

--- src/services/People/index.ts
+++ src/services/People/index.ts
@@ -2,8 +2,18 @@ import api from "../api";
 import type People from "@/types/People";
 
 abstract class PeopleApi {
-    static async get(id: number) :Promise<People> {
+    static async detail(id: number) :Promise<People> {
         const response = await api.get(`/people/${id}`);
+        return response.data;
+    }
+
+    static async list() :Promise<People[]> {
+        const response = await api.get(`/people`);
+        return response.data;
+    }
+
+    static async search(name: string) :Promise<People[]> {
+        const response = await api.get(`/people?search=${name}`);
         return response.data;
     }
 
--- src/services/People/people.spec.ts
+++ src/services/People/people.spec.ts
@@ -8,16 +8,36 @@ vi.mock('../api')
 const mockedAxios = api as Vi.Mocked<typeof api>;
 
 describe('PeopleApi', () => {
-    test('Ta pegando um usuario', async () => {
-        const mock = {
-            "name": "Luke Skywalker", "height": "172", "mass": "77", "hair_color": "blond", "skin_color": "fair", "eye_color": "blue", "birth_year": "19BBY", "gender": "male", "homeworld": "https://swapi.dev/api/planets/1/", "films": [ "https://swapi.dev/api/films/1/", "https://swapi.dev/api/films/2/", "https://swapi.dev/api/films/3/", "https://swapi.dev/api/films/6/" ], "species": [], "vehicles": [ "https://swapi.dev/api/vehicles/14/", "https://swapi.dev/api/vehicles/30/" ], "starships": [ "https://swapi.dev/api/starships/12/", "https://swapi.dev/api/starships/22/" ], "created": "2014-12-09T13:50:51.644000Z", "edited": "2014-12-20T21:17:56.891000Z", "url": "https://swapi.dev/api/people/1/"
-        }
+    test('Detalhes de Personagem', async () => {
+        const mock = require('../../mock/people.json')
+
+        mockedAxios.get.mockResolvedValue({ data: mock.results[1] })
+
+        const character = await PeopleApi.detail(1)
+
+        expect(api.get).toHaveBeenCalledWith('/people/1')
+        expect(character).toStrictEqual(mock.results[1])
+    })
+
+    test('Lista de personagens', async () => {
+        const mock = require('../../mock/people.json')
 
         mockedAxios.get.mockResolvedValue({ data: mock })
 
-        const people = await PeopleApi.get(1)
+        const people = await PeopleApi.list()
 
-        expect(api.get).toHaveBeenCalledWith('/people/1')
+        expect(api.get).toHaveBeenCalledWith('/people')
+        expect(people).toStrictEqual(mock)
+    })
+
+    test('Pesquisa de personagens', async () => {
+        const mock = require('../../mock/people.json')
+
+        mockedAxios.get.mockResolvedValue({ data: mock })
+
+        const people = await PeopleApi.search('Luke')
+
+        expect(api.get).toHaveBeenCalledWith('/people?search=Luke')
         expect(people).toStrictEqual(mock)
     })
 })
