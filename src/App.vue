45d106393a2f6ce3978ba75310264995ded60334
André Luis dos Santos <alsss.ads@gmail.com>
2023-03-11 23:17:05 -0300

    feat: limpa css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
+<template>
+  <section>
+    <TheNavBar />
+    <main>
+      <RouterView />
+    </main>
+  </section>
+</template>
+
 <script setup lang="ts">
+import TheNavBar from './components/TheNavBar.vue'
 import { RouterView } from 'vue-router'
 
 
 </script>
 
-<template>
-
-  <RouterView />
-</template>
-
 <style scoped>
-
+section {
+  margin: 0;
+  padding: 0;
+  background-color: #333;
+  color: #fff;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+}
 </style>
